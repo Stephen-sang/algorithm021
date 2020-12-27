8d6d70afb369e97ca90d9887b5bfe2e30aebcc71
Stephen-sang <wyyxsgh@163.com>
2020-12-28 02:00:48 +0800

    week4

---
+++ Week_04/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,12 @@
+public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int tmp;
+        for (int i = 1; i < prices.length; i++) {
+            tmp = prices[i] - prices[i - 1];
+            if (tmp > 0) profit += tmp;
+        }
+        return profit;
+
+    }
+}
